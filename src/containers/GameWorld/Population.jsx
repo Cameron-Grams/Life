ed557e7cdbe2fab53fab421da87f12e5d3524b3d
Cameron <cameron.grams.2@gmail.com>
2018-06-04 08:00:53 -0400

    working population, bring into control logic

--- src/containers/GameWorld/Population.jsx
+++ src/containers/GameWorld/Population.jsx
@@ -1,20 +1,38 @@
 import React from 'react';
 import Creature from './Creature'; 
 
+const newRow = () => {
+    const newArray = Array( 20 );
+
+    for ( let i = 0; i < newArray.length; i++ ){
+        newArray[ i ] = Math.floor( Math.random() * 2 );
+    }
+    console.log( newArray );
+    return newArray;
+}
+
 const Population = ( props ) => {
+
+    let outputArray = [];
+
+    const arrayOfRows = Array( 20 );
+
+    for ( let j = 0; j < 20; j++ ){
+        arrayOfRows[ j ] = newRow();
+    }
  
-    const populationArray = [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ];
+    for ( let k = 0; k < 20; k++ ){
+        let innerArray = [];
+        for ( let m = 0; m < 20; m++ ){
+            const creatureValue = arrayOfRows[ k ][ m ];
+            innerArray[ m ] = <Creature key={ m } status={ creatureValue } xLocation={ m * 10 } yLocation={ k * 10 } />
+        }
+        outputArray.push( innerArray );
+    }
 
-    let populatedRow = populationArray.map( ( details, index ) => {
-        return( 
-            <Creature status={ details } xLocation={ index * 10 } yLocation={ 0 } />
-        )
-    })
-
-    return(
-         populatedRow 
+   return(
+         outputArray 
     )
 };
 
 export default Population;
-
